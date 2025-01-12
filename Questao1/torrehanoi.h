bd5b2a44250c0fab0b2523a0ae99058398d7d8e7
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-01-12 15:59:29 -0300

    Create torrehanoi.h

---
+++ Questao1/torrehanoi.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Pinos 3
+#define Discos 4
+#define Possibilidades 81 // Pinos^Discos = 3^4
+
+typedef struct
+{
+    int posicao[Discos]; // Posição dos Discos em relação aos Pinos
+}vertice;
+
+int movimento_valido(vertice v1, vertice v2); 
+void gerar_matriz_adjacencia(vertice *grafo, int matriz[][Possibilidades]);
+void exibir_matriz(vertice *grafo);
+
+
