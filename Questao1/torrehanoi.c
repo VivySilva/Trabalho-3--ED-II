1d0bb84038666ee61a7fd24b38a86680fa7660a8
suehtamxx <eldermatheus60@gmail.com>
2025-01-12 23:39:34 -0300

    Update torrehanoi.c

--- Questao1/torrehanoi.c
+++ Questao1/torrehanoi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "torrehanoi.h"
+#include <limits.h>
+#include <time.h>
 
 int movimento_valido(vertice v1, vertice v2)
 {
@@ -39,13 +41,13 @@ void gerar_matriz_adjacencia(vertice *grafo, int matriz[][Possibilidades])
 {
     int disco, atribuido;
     
-    for(int posicao = 0; posicao < Possibilidades; posicao++)
+    for(int atual = 0; atual < Possibilidades; atual++)
     {
-        atribuido = posicao;
+        atribuido = atual;
         for(disco = 0; disco < Discos; disco++)
         {
-            grafo[posicao].posicao[posicao] = atribuido % Discos + 1;
-            atribuido /= Discos;
+            grafo[atual].posicao[disco] = atribuido % Pinos + 1;
+            atribuido /= Pinos;
         }
     }
 
@@ -53,7 +55,7 @@ void gerar_matriz_adjacencia(vertice *grafo, int matriz[][Possibilidades])
     {
         for(int amigo = 0; amigo < Possibilidades; amigo++)
         {
-            if(movimento_valido(grafo[atual], grafo[amigo])) matriz[atual][amigo] = 1; // Movimento Válido
+            if(movimento_valido(grafo[atual], grafo[amigo]) == 1) matriz[atual][amigo] = 1; // Movimento Válido
             else matriz[atual][amigo] = 0; //Movimento Inválido
         }
     }
@@ -61,13 +63,70 @@ void gerar_matriz_adjacencia(vertice *grafo, int matriz[][Possibilidades])
 
 void exibir_matriz(vertice *grafo)
 {
-    for(int atual = 0; atual > Possibilidades; atual++)
+    for(int atual = 0; atual < Possibilidades; atual++)
     {
-        printf("Vet %d:", atual);
+        printf("Vet %.2d:", atual);
         for(int disco = 0; disco < Discos; disco++)
             printf(" %d", grafo[atual].posicao[disco]);
         printf("\n");
     }
+}
+
+void dijkstra(int matriz[][Possibilidades], int inicio, int fim)
+{
+    int distancias[Possibilidades];
+    int visitados[Possibilidades], i, contador, v;
+
+    for (i = 0; i < Possibilidades; i++)
+    {
+        distancias[i] = INT_MAX;
+        visitados[i] = 0;
+    }
+
+    distancias[inicio] = 0;
+
+    for (contador = 0; contador < Possibilidades - 1; contador++)
+    {
+        int u = -1;
+
+        for (i = 0; i < Possibilidades; i++)
+        {
+            if (!visitados[i] && (u == -1 || distancias[i] < distancias[u]))
+            {
+                u = i;
+            }
+        }
+
+        visitados[u] = 1;
+
+        for (v = 0; v < Possibilidades; v++)
+        {
+            if (!visitados[v] && matriz[u][v] && distancias[u] != INT_MAX &&
+                distancias[u] + matriz[u][v] < distancias[v])
+            {
+                distancias[v] = distancias[u] + matriz[u][v];
+            }
+        }
+    }
+
+    printf("Menor caminho de %d para %d: %d\n", inicio, fim, distancias[fim]);
+}
+
+void menor_caminho(int matriz[][Possibilidades])
+{
+    int inicio = 0;
+    int fim = Possibilidades - 1;
+
+    clock_t startVerticies_time, end_time;
+    double time;
+
+    startVerticies_time = clock();
+    dijkstra(matriz, inicio, fim);
+    end_time = clock();
+
+    time = (double)(end_time - startVerticies_time) / CLOCKS_PER_SEC;
+
+    printf("Tempo gasto - Dijkstra: %lf ms\n", time * 1000);
 }
 
 int main()
@@ -76,7 +135,8 @@ int main()
     int matriz_adjacencia[Possibilidades][Possibilidades];
 
     gerar_matriz_adjacencia(grafo, matriz_adjacencia);
-    exibir_matriz(grafo);  
-     
+    //exibir_matriz(grafo);  
+    
+    menorcaminho(matriz_adjacencia);
     return 0;
 }
