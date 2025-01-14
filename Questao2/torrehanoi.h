04929a1fc6c60ec3d6cd443f84f8847f22e0c47f
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-01-13 22:26:57 -0300

    Update torrehanoi.h

--- Questao2/torrehanoi.h
+++ Questao2/torrehanoi.h
@@ -15,4 +15,4 @@ void gerar_matriz_adjacencia(vertice *grafo, int matriz[][Possibilidades]);
 void exibir_matriz(vertice *grafo);
 
 void menor_caminho(int matriz[][Possibilidades]);
-void fordbellman(int matriz[][Possibilidades], int inicio, int fim);
+int fordbellman(int matriz[][Possibilidades], int inicio, int fim);
